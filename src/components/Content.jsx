feba9427036061a268871e0bba6b69afcbb8b624
AjayKumarNaidu <iajaykumar3377@gmail.com>
2024-12-24 08:33:33 +0530

    added bg style in react

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,25 @@
+import bgImg from '/og-1.jpeg'
+
 export default function Content(props){
+
+  const styles = {
+    display: 'inline-block',
+    padding: '0px',
+    fontSize: '15rem',
+    backgroundImage: `url(${bgImg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'top',
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    color: 'transparent',
+    fontFamily: 'sans-serif',
+    fontWeight: '800',
+  };
+  
+
   return (
     <div className="content" onClick={()=>{ props.intoPhase(props.index) }}>
-      <p>{props.title}</p>
+      <p style={styles}>{props.title}</p>
     </div>
   );
 }
